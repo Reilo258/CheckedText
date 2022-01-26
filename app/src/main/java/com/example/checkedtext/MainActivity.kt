b1f8bdaae1b701fd802dc2ad6577264f5c974733
Reilo258 <olivier258@wp.pl>
2022-01-26 07:54:52 +0100

    Ziemniaki, mąka, cukier i żelki

--- app/src/main/java/com/example/checkedtext/MainActivity.kt
+++ app/src/main/java/com/example/checkedtext/MainActivity.kt
@@ -32,5 +32,71 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT).show()
             }
         }
+
+        val CTView2 = findViewById<CheckedTextView>(R.id.ctv2)
+        if (CTView2 != null) {
+            CTView2.isChecked = false
+            CTView2.setCheckMarkDrawable(
+                android.R.drawable.checkbox_off_background)
+
+            CTView2.setOnClickListener {
+                CTView2.isChecked = !CTView2.isChecked
+                CTView2.setCheckMarkDrawable(
+                    if (CTView2.isChecked)
+                        android.R.drawable.checkbox_on_background
+                    else
+                        android.R.drawable.checkbox_off_background)
+
+                val msg = getString(R.string.msg_shown2)+ " " +
+                        getString(if (CTView2.isChecked)
+                            R.string.checked2 else R.string.unchecked2)
+                Toast.makeText(this@MainActivity, msg,
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val CTView3 = findViewById<CheckedTextView>(R.id.ctv3)
+        if (CTView3 != null) {
+            CTView3.isChecked = false
+            CTView3.setCheckMarkDrawable(
+                android.R.drawable.checkbox_off_background)
+
+            CTView3.setOnClickListener {
+                CTView3.isChecked = !CTView3.isChecked
+                CTView3.setCheckMarkDrawable(
+                    if (CTView3.isChecked)
+                        android.R.drawable.checkbox_on_background
+                    else
+                        android.R.drawable.checkbox_off_background)
+
+                val msg = getString(R.string.msg_shown3)+ " " +
+                        getString(if (CTView3.isChecked)
+                            R.string.checked3 else R.string.unchecked3)
+                Toast.makeText(this@MainActivity, msg,
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val CTView4 = findViewById<CheckedTextView>(R.id.ctv4)
+        if (CTView4 != null) {
+            CTView4.isChecked = false
+            CTView4.setCheckMarkDrawable(
+                android.R.drawable.checkbox_off_background)
+
+            CTView4.setOnClickListener {
+                CTView4.isChecked = !CTView4.isChecked
+                CTView4.setCheckMarkDrawable(
+                    if (CTView4.isChecked)
+                        android.R.drawable.checkbox_on_background
+                    else
+                        android.R.drawable.checkbox_off_background)
+
+                val msg = getString(R.string.msg_shown4)+ " " +
+                        getString(if (CTView4.isChecked)
+                            R.string.checked else R.string.unchecked)
+                Toast.makeText(this@MainActivity, msg,
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
